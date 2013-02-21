a7b002168518ac1054f4b0ccb8739d740ec5a54a
Teddy <ted107mk@gmail.com>
2013-02-21 09:17:22 -0500

    Headers Done

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def overviews
+  end
+
+  def descriptions
+  end
+
+  def contact
+  end
+
+  def resources
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 EngineeringExploration::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/overviews"
+
+  get "static_pages/descriptions"
+
+  get "static_pages/contact"
+
+  get "static_pages/resources"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
